4030056db52c91485fa062369599d6b6f8783348
aw <axel.wiemert@epitech.eu>
2023-01-23 15:11:39 +0100

    Add even_or_odd.c

---
+++ C/even_or_odd.c
@@ -0,0 +1,14 @@
+/*
+** CODEWARS PROJECT, 2023
+** same_case.c
+** Author:
+** pasdepseudo
+*/
+
+const char *even_or_odd (int number)
+{
+  if (number % 2 == 0)
+    return "Even";
+  else
+    return "Odd";
+}
